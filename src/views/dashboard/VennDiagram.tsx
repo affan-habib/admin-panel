0367143a690f6702c7dae3b8b4d35c19d293758c
mrakib007 <mrakibhasan199744@gmail.com>
2023-11-19 10:08:02 +0600

    Solving git problem

--- src/views/dashboard/VennDiagram.tsx
+++ src/views/dashboard/VennDiagram.tsx
@@ -3,23 +3,6 @@ import React from 'react';
 const VennDiagram = () => {
   return (
     <div className="venn">
-      {/* <div className="circle orange">
-        <p>
-          <strong>Option Set 1</strong><br /> Option 1 OR Option 2
-        </p>
-      </div>
-
-      <div className="circle blue">
-        <p>
-          <strong>Option Set 1</strong><br /> Option 3 OR Option 4
-        </p>
-      </div>
-
-      <div className="joined">
-        <p>
-          <strong>Joined:</strong> <br /> Use Option Set 1 AND Use Option Set 2
-        </p>
-      </div> */}
     </div>
   );
 };
