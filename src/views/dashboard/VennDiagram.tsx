5c73bac671639706489e82c68168350ead9718c3
mrakib007 <mrakibhasan199744@gmail.com>
2023-11-19 09:40:56 +0600

    Testing

---
+++ src/views/dashboard/VennDiagram.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './VennDiagram.css'; // Import the CSS file
+
+const VennDiagram = () => {
+  return (
+    <div className="venn">
+      {/* <div className="circle orange">
+        <p>
+          <strong>Option Set 1</strong><br /> Option 1 OR Option 2
+        </p>
+      </div>
+
+      <div className="circle blue">
+        <p>
+          <strong>Option Set 1</strong><br /> Option 3 OR Option 4
+        </p>
+      </div>
+
+      <div className="joined">
+        <p>
+          <strong>Joined:</strong> <br /> Use Option Set 1 AND Use Option Set 2
+        </p>
+      </div> */}
+    </div>
+  );
+};
+
+export default VennDiagram;
+
+
+
