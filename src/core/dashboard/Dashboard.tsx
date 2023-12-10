43cdd0eada4031dfb44c0c11498315a9ecc5038e
mrakib007 <mrakibhasan199744@gmail.com>
2023-12-10 12:01:05 +0600

    Added image switch with language switch

--- src/core/dashboard/Dashboard.tsx
+++ src/core/dashboard/Dashboard.tsx
@@ -1,73 +1,3 @@
-// import React from 'react';
-// import { Box, Container, Grid } from '@mui/material';
-// import TopCards from 'views/dashboard/TopCards';
-// import InstituteList from 'views/dashboard/InstituteList';
-// import StackChart from 'views/dashboard/StackChart';
-// import PieChartDesign from 'views/dashboard/PieChartDesign';
-// import VennDiagram from 'views/dashboard/VennDiagram';
-
-// const Dashboard: React.FC = () => {
-//   return (
-//     <Container maxWidth="xl">
-//       <TopCards />
-//       <Grid container spacing={2}>
-//         <Grid item xs={12} md={8}>
-//           <InstituteList />
-//         </Grid>
-//         <Grid item xs={12} md={4} marginLeft={-3.5}>
-//           <VennDiagram />
-//         </Grid>
-//       </Grid>
-//       <Grid container>
-//         <Grid item xs={12} md={8} lg={8}>
-//           <StackChart />
-//         </Grid>
-//         <Grid item xs={12} md={4} lg={4} marginLeft={-2.2}>
-//           <PieChartDesign />
-//         </Grid>
-//       </Grid>
-//     </Container>
-//   );
-// };
-
-// export default Dashboard;
-
-
-// import React from 'react';
-// import { Box, Container, Grid } from '@mui/material';
-// import TopCards from 'views/dashboard/TopCards';
-// import InstituteList from 'views/dashboard/InstituteList';
-// import StackChart from 'views/dashboard/StackChart';
-// import PieChartDesign from 'views/dashboard/PieChartDesign';
-// import VennDiagram from 'views/dashboard/VennDiagram';
-
-// const Dashboard: React.FC = () => {
-//   return (
-//     <Container maxWidth="xl">
-//       <TopCards />
-//       <Grid container spacing={2}>
-//         <Grid item xs={12} md={8} lg={8}>
-//           <InstituteList />
-//         </Grid>
-//         <Grid item xs={12} md={4} lg={4}>
-//           <VennDiagram />
-//         </Grid>
-//       </Grid>
-//       <Grid container spacing={2} sx={{marginTop:'3px'}}>
-//         <Grid item xs={12} md={8} lg={8}>
-//           <StackChart />
-//         </Grid>
-//         <Grid item xs={12} md={4} lg={4}>
-//           <PieChartDesign />
-//         </Grid>
-//       </Grid>
-//     </Container>
-//   );
-// };
-
-// export default Dashboard;
-
-
 import React from 'react';
 import { Box, Container, Grid } from '@mui/material';
 import TopCards from 'views/dashboard/TopCards';
@@ -76,29 +6,33 @@ import StackChart from 'views/dashboard/StackChart';
 import PieChartDesign from 'views/dashboard/PieChartDesign';
 import VennDiagram from 'views/dashboard/VennDiagram';
 import { useTranslation } from 'react-i18next';
+import VennEnglish from 'views/dashboard/VennEnglish';
 const Dashboard: React.FC = () => {
-  
+
   const { t } = useTranslation();
+  const languagestate = localStorage.getItem('language')
+  console.log(languagestate)
   return (
     <Container maxWidth="xl">
-      <p style={{fontSize:'14px',fontWeight:'500', marginBottom:'8px' ,color:'rgba(244, 42, 65, 1)'}}>{t('adminPanel')}</p>
+      <p style={{ fontSize: '14px', fontWeight: '500', marginBottom: '8px', color: 'rgba(244, 42, 65, 1)' }}>{t('adminPanel')}</p>
       <TopCards />
       <Grid container spacing={2}>
         <Grid item xs={12} md={8} lg={8}>
           <InstituteList />
         </Grid>
         <Grid item xs={12} md={4} lg={4}>
-          <VennDiagram />
+          {languagestate == 'bn' && <VennDiagram />}
+          {languagestate == 'en' && <VennEnglish />}
         </Grid>
       </Grid>
       <Grid container spacing={2} sx={{ marginTop: '3px' }}>
-  <Grid item xs={12} md={8} lg={8} sx={{  }}>
-    <StackChart />
-  </Grid>
-  <Grid item xs={12} md={4} lg={4} sx={{  }}>
-    <PieChartDesign  />
-  </Grid>
-</Grid>
+        <Grid item xs={12} md={8} lg={8} sx={{}}>
+          <StackChart />
+        </Grid>
+        <Grid item xs={12} md={4} lg={4} sx={{}}>
+          <PieChartDesign />
+        </Grid>
+      </Grid>
 
 
     </Container>
---
+++ src/views/dashboard/VennEnglish.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import vennEng from '../../assets/vennEng.svg';
+import { Grid, Typography, useTheme } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+
+const VennEnglish
+ = () => {
+  const { t } = useTranslation();
+  const theme = useTheme();
+  return (
+    <Grid
+      
+      sx={{
+        backgroundColor: 'rgba(237, 244, 242, 1)',
+        borderRadius: '8px',
+       
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: "20px",
+        height:300,
+        marginTop: '10px', 
+        [theme.breakpoints.up('md')]: {
+          marginTop: '40px', 
+        },
+      }}
+    >
+      <Grid sx={{paddingBottom:"8px"}}>
+        <Typography style={{ color: 'rgba(21, 83, 19, 1)',fontSize:'17px',fontWeight:'500'}}>{t('venDiagram')}</Typography>
+      </Grid>
+      <Grid
+        sx={{
+          backgroundColor: 'rgba(237, 244, 242, 1)'
+        }}
+      >
+        <img
+          src={vennEng}
+          alt=""
+          style={{
+            maxWidth: '100%', // Set maximum width relative to its container
+            height:'auto'
+            
+          }}
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default VennEnglish
+;
