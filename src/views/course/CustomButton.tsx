ec189e9474b1ff2879b870dbc5e50ca1658ea8af
affan-habib <affan.habib44@gmail.com>
2023-12-10 15:48:39 +0600

    fixed

--- src/views/course/CustomButton.tsx
+++ src/views/course/CustomButton.tsx
@@ -1,31 +1,74 @@
-import React from 'react';
-import { Box, Button, Typography } from '@mui/material';
-import { Add } from '@mui/icons-material';
+import React, { ReactNode, MouseEvent } from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-const CustomBox = () => {
+interface CustomBoxProps {
+  icon: ReactNode;
+  title: string;
+  onClick: (event: MouseEvent<HTMLDivElement>) => void;
+  disabled?: boolean;
+}
+
+const CustomButton: React.FC<CustomBoxProps> = ({
+  icon,
+  title,
+  onClick,
+  disabled,
+}) => {
   return (
     <Box
-      border="1px solid #D0D0D0"
-      borderRadius={0}
-      width="100px"
-      height="100px"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
+      sx={{
+        width: '100px',
+        height: '100px',
+        border: '1px solid rgba(208, 208, 208, 1)',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        transition: 'background-color 0.3s',
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          color: 'white',
+          '& svg': {
+            color: 'white',
+          },
+          '& div:nth-of-type(2)': {
+            backgroundColor: 'primary.main',
+            color: 'white',
+          },
+        },
+      }}
+      onClick={onClick}
     >
-      <Button
-        style={{ width: '100%', borderRadius: 0 }}
-        variant="outlined"
-        startIcon={<Add />}
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: '12px',
+        }}
       >
-        {/* Icon */}
-        <Add fontSize="small" />
-        {/* Title */}
-        <Typography variant="subtitle2">Button Title</Typography>
-      </Button>
+        {icon}
+      </Box>
+      <Box
+        sx={{
+          height: '50%',
+          width: '100%',
+          backgroundColor: 'rgba(217, 217, 217, 1)',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          transition: 'background-color 0.3s',
+          '&:hover': {
+            backgroundColor: 'primary.main',
+            color: 'white',
+          },
+        }}
+      >
+        <Typography>{title}</Typography>
+      </Box>
     </Box>
   );
 };
 
-export default CustomBox;
+export default CustomButton;
