93a9ee4db73386817edbc71b86b5c3388feff1e9
affan-habib4 <affan@siin.shop>
2023-12-11 19:20:35 +0600

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust this pattern based on your project structure
+  ],
   theme: {
     extend: {},
   },
