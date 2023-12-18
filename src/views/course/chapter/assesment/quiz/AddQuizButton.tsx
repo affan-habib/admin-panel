443a7f8fcbc4864ade95642dcfaf7806ebe13f5c
mrakib007 <mrakibhasan199744@gmail.com>
2023-12-18 11:10:08 +0600

    Fixing the modal styles

--- src/views/course/chapter/assesment/quiz/AddQuizButton.tsx
+++ src/views/course/chapter/assesment/quiz/AddQuizButton.tsx
@@ -12,11 +12,14 @@ import {
   FormControl,
   RadioGroup,
   Radio,
+  IconButton,
 } from '@mui/material';
 import { Formik, Form } from 'formik';
 import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
 
 const AddQuizButton: React.FC<any> = ({ assessmentId }) => {
   const [open, setOpen] = useState(false);
@@ -64,15 +67,19 @@ const AddQuizButton: React.FC<any> = ({ assessmentId }) => {
 
   return (
     <>
-      <Button variant="contained" onClick={handleOpen}>
-        Open Modal
+      <Button
+        onClick={handleOpen}
+        sx={{ marginLeft: '7px' }}
+        variant="outlined"
+        startIcon={<AssignmentOutlinedIcon />}
+      >
+        কুইজ
       </Button>
       <Modal open={open} onClose={handleClose}>
         <Box
           sx={{
             position: 'absolute',
-            width: 1300,
-            minHeight: 750,
+            maxWidth: 'xl',
             bgcolor: 'background.paper',
             borderRadius: '8px',
             boxShadow: 24,
@@ -82,12 +89,22 @@ const AddQuizButton: React.FC<any> = ({ assessmentId }) => {
             transform: 'translate(-50%, -50%)',
           }}
         >
-          <Typography variant="h6" component="h2" gutterBottom>
-            Modal Content
-          </Typography>
+          <Grid sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            borderBottom: '2px solid rgba(208, 208, 208, 1)'
+          }}>
+            <Typography color="primary" variant="h6" p={2}>
+              এমসিকিউ উইথ পিকচার যোগ করুন (কারবালা প্রান্তর)
+            </Typography>
+            <IconButton onClick={handleClose} color="error" >
+              <HighlightOffIcon />
+            </IconButton>
+          </Grid>
           <Formik initialValues={{ option: 'option1' }} onSubmit={handleSubmit}>
             <Form>
-              <Box display="flex" justifyContent="" gap={8}>
+              <Box my={2} display="flex" justifyContent="" gap={8}>
                 <FormControlLabel
                   value="option1"
                   control={<Checkbox />}
@@ -100,34 +117,53 @@ const AddQuizButton: React.FC<any> = ({ assessmentId }) => {
                 />
               </Box>
 
-              <Box mt={4} border="1px dashed #000" p={2}>
+              <Box mt={2} border="1px dashed rgba(208, 208, 208, 1)" p={2}>
                 <Typography variant="h6" gutterBottom>
                   Form with Text Editor
                 </Typography>
                 <form>
-                  <Box mb={2}>
-                    <FormControl component="fieldset">
-                      <RadioGroup row value={value} onChange={handleChange}>
-                        <FormControlLabel
-                          value="option1"
-                          control={<Radio />}
-                          label="Option 1"
-                        />
-                        <FormControlLabel
-                          value="option2"
-                          control={<Radio />}
-                          label="Option 2"
-                        />
-                      </RadioGroup>
-                    </FormControl>
+                  <Box mb={2} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', gap: '20px' }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
+                      <Typography>কুইজের প্রশ্নের ধরন:</Typography>
+                      <FormControl component="fieldset">
+                        <RadioGroup row value={value} onChange={handleChange}>
+                          <FormControlLabel
+                            value="option1"
+                            control={<Radio />}
+                            label="Option 1"
+                          />
+                          <FormControlLabel
+                            value="option2"
+                            control={<Radio />}
+                            label="Option 2"
+                          />
+                        </RadioGroup>
+                      </FormControl>
 
-                    <ReactQuill
-                      id="editor"
-                      value={editorHtml}
-                      onChange={handleChange}
-                      style={{ height: '100px' }}
-                    />
+                    </Box>
+
+                    <Box>
+                      <FormControl fullWidth variant="outlined" size="small" sx={{ marginTop: '12px' }}>
+                        <OutlinedInput
+                          startAdornment={
+                            <InputAdornment position="start">
+                              <Typography>মার্ক দিন</Typography>
+                            </InputAdornment>
+                          }
+                        />
+                      </FormControl>
+
+                    </Box>
+
+
                   </Box>
+
+                  <ReactQuill
+                    id="editor"
+                    value={editorHtml}
+                    onChange={handleChange}
+                    style={{ height: '100px' }}
+                  />
 
                   <Grid container spacing={2} mt={5}>
                     {items.map((item) => (
