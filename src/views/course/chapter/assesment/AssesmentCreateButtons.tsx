08c9969f72ae07d8624d60484f1fa8e141a4ffb6
mrakib007 <mrakibhasan199744@gmail.com>
2023-12-21 16:02:56 +0600

    Data fecthing for button and assessment types

--- src/views/course/chapter/assesment/AssesmentCreateButtons.tsx
+++ src/views/course/chapter/assesment/AssesmentCreateButtons.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Box } from '@mui/material';
 import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
 import AddQuizButton from './quiz/AddQuizButton';
 import AddMatchingButton from './matching/AddMatchingButton';
 import IntegratedQuestionButton from './integrated-question/IntegratedQuestionButton';
 import AddIcon from '@mui/icons-material/Add';
+import axios from 'axios';
+import { apiBaseUrl } from 'config';
 
 const AssesmentCreateButtons: React.FC<any> = ({ assessmentId }) => {
-  //   console.log(assessmentId);
+  const token = localStorage.getItem('token');
+  const [data, setData] = useState([]);
+  // const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${apiBaseUrl}/quiz-types`,{
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        // setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData(); 
+  }, []);
+
+   console.log(data);
   return (
     <Box
       sx={{
